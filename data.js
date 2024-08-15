a33ca734f5607dae837ffc50b38151f49adef3de
pablo-renan17 <pablonichterwistz@outlook.com>
2024-08-14 21:58:56 -0300

    finalizacao aula 14/08

--- data.js
+++ data.js
@@ -3,36 +3,42 @@ export const data = [
         id: "0",
         nome: "Camisa banda Isengard",
         descricao: "100% algodão cor preta",
-        preco: "79,90"
+        preco: "79,90",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAh8HsZRejQXUO9yd2FGqFZnhXc81wMrxFYg&s"
     },
     {
         id: "1",
         nome: "Camisa banda Nargaroth",
         descricao: "100% algodão cor preta",
-        preco: "89,90"
+        preco: "89,90",
+        img: "https://http2.mlstatic.com/D_NQ_NP_712238-MLB46778645411_072021-O.webp"
     },
     {
         id: "2",
         nome: "Camisa banda Burzum",
         descricao: "100% algodão cor branca",
-        preco: "69,90"
+        preco: "69,90",
+        img: "https://http2.mlstatic.com/D_NQ_NP_2X_679934-MLB72315628616_102023-F.webp"
     },
     {
         id: "3",
         nome: "Camisa banda Pantera",
         descricao: "100% algodão cor preta",
-        preco: "89,90"
+        preco: "89,90",
+        img: "https://images.tcdn.com.br/img/img_prod/1147522/camiseta_pantera_vulgar_display_of_power_148357_1_09bb18d1b51ed788e6bfda905bf5614c.jpeg"
     },
     {
         id: "4",
-        nome: "Camisa banda MeowTallica",
+        nome: "Camisa banda Iron Meowden",
         descricao: "100% algodão cor preta",
-        preco: "79,90"
+        preco: "79,90",
+        img: "https://acdn.mitiendanube.com/stores/774/002/products/20220827_123900-9d5f3fbf2c14ed533116616148792261-1024-1024.webp"
     },
     {
         id: "5",
         nome: "Camisa banda Cannibal Cat",
         descricao: "100% algodão cor preta",
-        preco: "49,90"
+        preco: "49,90",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCq7fFgQdKQN85WOvDR4arAMl-x4Z_i_TuLw&s"
     },
 ]
--- script.js
+++ script.js
@@ -1,45 +1,78 @@
-import {data} from "./data.js";
+import { data } from "./data.js";
 console.log(data);
 
 let carrinho = [];
 let carrinhoItemId = 0;
-function montarListaDeItens(){
-    data.forEach((item)=>{
+
+function montarListaDeItens() {
+    data.forEach((item) => {
         const ulCompra = document.querySelector(".compra");
-        ulCompra.insertAdjacentHTML("beforeend",`
-            <li>
-                <p>${item.nome}</p>
-                <button id="btn-compra-${item.id}">Adicionar ao carrinho</button>
-            </li>
-            `);
-        const btnCompra = document.querySelector(`#btn-compra-${item.id}`);
-        btnCompra.addEventListener("click", ()=>{
-            console.log(item);
-            carrinho.push([item, carrinhoItemId]);
-            adicionarItemCarrinho(item);
+        ulCompra.insertAdjacentHTML("beforeend", `
+            <li class="li-compra">
+                        <div class="d-flex">
+                            <div>
+                                <img height="75" src="${item.img}">
+                            </div>
+                            <div>
+                                <p>${item.nome}</p>
+                                <p>Qnt</p>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-center">
+                            <button class="btn-acao btn-compra-${item.id}">Adicionar ao carrinho</button>
+                        </div>
+                    </li>
+        `);
+
+        const btnCompra = document.querySelector(`.btn-compra-${item.id}`);
+        btnCompra.addEventListener("click", () => {
+            carrinhoItemId++; // Incrementa antes de adicionar ao carrinho
+            let temItem = carrinho.find((car)=>car.id===item.id);
+            if(temItem){
+                item.qnt += 1;
+            }else{
+                item.qnt = 1;
+                carrinho.push(item); // Armazena item com id exclusivo
+            }
+            console.log(carrinho)
+            adicionarItemCarrinho(); // Atualiza o carrinho
         });
-    })
+    });
 }
 montarListaDeItens();
 
-function adicionarItemCarrinho(){
+function adicionarItemCarrinho() {
+    // Resetar a div
     const carrinhoUl = document.querySelector(".carrinho-ul");
     carrinhoUl.innerHTML = "";
-    carrinho.forEach((item)=>{
-        carrinhoUl.insertAdjacentHTML("beforeend",`
-            <li id="li-carrinho-${carrinhoItemId}">
-                <p>${item[0].nome}</p>
-                <button id="btn-carrinho-${carrinhoItemId}">Remover</button>
-            </li>
-            `)
-        const btnRemover = document.querySelector(`#btn-carrinho-${carrinhoItemId}`);
-        btnRemover.addEventListener("click", ()=>{
-            let carrinhoLi = document.querySelector(`#li-carrinho-${carrinhoItemId}`);
-            carrinhoLi.remove();
-            carrinho = carrinho.filter(teste=> !teste.includes(carrinhoItemId));
 
-            
+    carrinho.forEach((item) => { // Usar o id exclusivo para cada item
+        carrinhoUl.insertAdjacentHTML("beforeend", `
+            <li class="li-compra remove-id-${item.id}">
+                        <div class="d-flex">
+                            <div>
+                                <img height="75" src="${item.img}">
+                            </div>
+                            <div>
+                                <p>${item.nome}</p>
+                                <p>${item.qnt}</p>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-center">
+                            <button class="btn-acao btn-remove-id${item.id}">Remover</button>
+                        </div>
+                    </li>
+
+        `);
+
+        const btnRemover = document.querySelector(`.btn-remove-id${item.id}`);
+        btnRemover.addEventListener("click", () => {
+            let carrinhoLi = document.querySelector(`.remove-id-${item.id}`);
+            carrinhoLi.remove();
+            carrinho = carrinho.filter(teste => teste.id !== item.id); // Remove o item do carrinho pelo id
+            if(carrinho = []){
+                carrinhoItemId = 0;
+            }
         });
-       
-    })
+    });
 }
